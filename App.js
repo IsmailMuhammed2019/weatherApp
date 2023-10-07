c5101a5c577352fb3900bd79c4ab944f24742598
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 13:35:34 +0100

    customize layout for environment component

--- App.js
+++ App.js
@@ -3,17 +3,28 @@ import {View, Text, SafeAreaView, StyleSheet} from "react-native"
 
 const App = () => {
   return(
-    <SafeAreaView>
+    <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text style={{color: "#fff", margin: 20}}>This is my test weather app</Text>
+        <Text style={styles.text}>This is my test weather app</Text>
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create ({
+  wrapper: {
+    flex: '1'
+  },
   container: {
-    backgroundColor: "skyblue"
+    backgroundColor: "skyblue",
+    flex: '1',
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "900",
+    fontSize: '100',
+    color: '#fff'
   }
 })
 export default App;
