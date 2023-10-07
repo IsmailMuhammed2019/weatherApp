83e5afd3521e005858d71bed6317eeac2fab2a63
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 17:33:33 +0100

    create style for country and city

--- src/screens/City.js
+++ src/screens/City.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {Text, SafeAreaView, StyleSheet, ImageBackground, StatusBar } from 'react-native'
 
 const City = () => {
-    const {container, imageStyle} = styles
+    const {container, imageStyle, cityName, countryName} = styles
     return (
         <SafeAreaView style={container}>
             <ImageBackground source={require('../../assets/city.jpg')} style={imageStyle}>
-
+                <Text style={cityName}>London</Text>
+                <Text style={countryName}>Uk</Text>
             </ImageBackground>
         </SafeAreaView>
     )
@@ -19,6 +20,20 @@ const styles = StyleSheet.create ({
     },
     imageStyle: {
         flex: 1
+    },
+    cityName: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    countryName: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white'
     }
 })
 
