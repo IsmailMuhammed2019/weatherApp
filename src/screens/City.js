6fe0448080f2d382b581616543e870739023dec8
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 18:42:59 +0100

    create city codes

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import {Text, SafeAreaView, StyleSheet, ImageBackground, StatusBar } from 'react-native'
-
+import {View, Text, SafeAreaView, StyleSheet, ImageBackground, StatusBar } from 'react-native'
+import { Feather } from '@expo/vector-icons';
 const City = () => {
-    const {container, imageStyle, cityName, countryName} = styles
+    const {sunStatus, sunrise, container, imageStyle, cityName, countryName, cityText, population, populationText} = styles
     return (
         <SafeAreaView style={container}>
             <ImageBackground source={require('../../assets/city.jpg')} style={imageStyle}>
-                <Text style={cityName}>London</Text>
-                <Text style={countryName}>Uk</Text>
+                <Text style={[cityName, cityText ]}>London</Text>
+                <Text style={[countryName, cityText]}>Uk</Text>
+                <View>
+                    <Feather name={'user'} size={50} color={'red'} />
+                    <Text style={population}>Population</Text>
+                    <Text style={populationText}>8,000</Text>
+                </View>
+                <View style={sunStatus}>
+                    <Feather name={'sunrise'} size={50} color={'white'} />
+                    <Text style={sunrise}> 10:46:58am</Text>
+                    <Feather name={'sunset'} size={50} color={'white'} />
+                    <Text style={sunrise}> 17:28:15am</Text>            
+                </View>
+
             </ImageBackground>
         </SafeAreaView>
     )
@@ -22,18 +34,40 @@ const styles = StyleSheet.create ({
         flex: 1
     },
     cityName: {
+        fontSize: 40,
+    },
+    countryName: {
+        fontSize: 30,
+    },
+
+    cityText: {
         justifyContent: 'center',
         alignSelf: 'center',
-        fontSize: 40,
         fontWeight: 'bold',
         color: 'white'
     },
-    countryName: {
+    population: {
+        flexDirection: 'row',
+        alignItems: 'center',
         justifyContent: 'center',
-        alignSelf: 'center',
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: 'white'
+        marginTop: 30
+    },
+    populationText: {
+        fontSize: 25,
+        marginLeft: 7.5,
+        color: 'red',
+        fontWeight: 'bold'
+    },
+    sunStatus: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 30
+    },
+    sunrise: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
 
