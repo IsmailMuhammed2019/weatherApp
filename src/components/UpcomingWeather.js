eacc2694d343f87ca776d6cee2affb16e309b4bb
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 14:51:36 +0100

    add static data

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView} from "react-native"
+import { View, Text, StyleSheet, SafeAreaView, FlatList} from "react-native"
+
+const DATA = [
+    {
+        dt_txt: "2023-02-18 12:00:00",
+        main: {
+            temp_max: 8.55,
+            temp_min: 7.55
+        },
+        weather: [
+            {
+                main: 'Clear'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 15:00:00",
+        main: {
+            temp_max: 9.22,
+            temp_min: 6.39
+        },
+        weather: [
+            {
+                main: 'Clouds'
+            }
+        ]
+    },
+    {
+        dt_txt: "2023-02-18 18:00:00",
+        main: {
+            temp_max: 12.23,
+            temp_min: 10.54
+        },
+        weather: [
+            {
+                main: 'Rain'
+            }
+        ]
+    }
+]
 
 const UpcomingWeather = () => {
     return(
         <SafeAreaView style={styles.wrapper}>
             <View style={styles.container}>
                 <Text>The current upcoming weather</Text>
+                <FlatList
+                data=render{}
+                />
             </View>
         </SafeAreaView>
     )
