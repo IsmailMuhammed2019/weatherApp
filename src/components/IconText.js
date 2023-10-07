328f20f214e0b5483a4879da52839b7de1ec819f
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 19:45:01 +0100

    adding codes to iconitem components

--- src/components/IconText.js
+++ src/components/IconText.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-const IconText = () => {
+const IconText = (props) => {
+    const { iconName, iconColor, bodyText, bodyTextStyles } = props
+    const {textTheme} = styles
   return (
-    <div>IconText</div>
+    <View style={population}>
+        <Feather name={iconName} size={50} color={iconColor} />
+        <Text style={[textTheme, bodyTextStyles]}>{bodyText}</Text>
+    </View>
   )
 }
 
 const styles = ({
-
+  textTheme: {
+    fontWeight: 'bold'
+  }
 })
 
 export default IconText
