660482ef58efae42206050123a155d597e76d851
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 13:28:11 +0100

    importing stylesheet and creating simple style

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import {View, Text, SafeAreaView} from "react-native"
+import {View, Text, SafeAreaView, StyleSheet} from "react-native"
 
 const App = () => {
   return(
     <SafeAreaView>
-      <View style={{backgroundColor: 'orange'}}>
+      <View style={styles.container}>
         <Text style={{color: "#fff", margin: 20}}>This is my test weather app</Text>
       </View>
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create ({
+  container: {
+    backgroundColor: "skyblue"
+  }
+})
 export default App;
