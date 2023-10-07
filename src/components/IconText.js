227ab4ad5209b4953e2a8b9245dba45de3f9dea0
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-07 19:33:55 +0100

    setup boilerplate for iconsitems

---
+++ src/components/IconText.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const IconText = () => {
+  return (
+    <div>IconText</div>
+  )
+}
+
+const styles = ({
+
+})
+
+export default IconText
