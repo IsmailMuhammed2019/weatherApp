f475ea043affb40d976d13034857547794d487d2
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-10-10 11:20:16 +0100

    set up boiler plate for row text component

---
+++ src/components/RowText.js
@@ -0,0 +1,13 @@
+import React form 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const RowText = () => {
+    return (
+
+    )
+}
+
+const styles=StyleSheet.create ({
+
+})
+export default RowText
